055f133b3e635712a4fe4eeb7f239d8b517ed873
mjiang95 <minniejiang2013@gmail.com>
2021-11-06 09:42:15 -0700

    Update Upload page with corresponding functionality

--- src/Page/Upload/upload.jsx
+++ src/Page/Upload/upload.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import "./upload.scss";
+import { Component } from 'react';
+import { Link } from "react-router-dom";
 
-function Upload() {
+class Upload extends Component {
+
+    clickHandler() {
+        alert ("Upload successful!");
+    }
+
+    render () {
+
     return (
+        <>
         <section className= "upload-video">
             <div className= "upload-video-section">
                 <h1 className="upload-video__title">
@@ -12,22 +22,27 @@ function Upload() {
             <div className= "upload-video__components">
                     <div className="upload-video__main-component">
                         <h2 className="upload-video__subtitle">VIDEO THUMBNAIL</h2>
-                        <video className="upload-video__video-player" src=""></video>
                     </div>
                     <div className= "upload-video__details">
+                        <video className="upload-video__video-player" src=""></video>
                         <label className="input-title__label" htmlFor="input__title">TITLE YOUR VIDEO</label>
                         <input type="text" className="input__title" name="input__title" placeholder="Add a title to your video"/>
                         <label className="input-description__label"htmlFor="input__description">ADD A VIDEO DESCRIPTION</label>
                         <input type="text" className="input__description"name="input__description" placeholder="Add a description to your video"/>
                     </div>
-            </div>
+                </div>
                     <div className= "upload-video__cta">
-                        <input className="publish" type="button" value="PUBLISH"/>
+                        <div>
+                        <li>
+                        <Link to="/" className="publish">
+                        <button ahref="http://localhost:3002/"  onClick={this.clickHandler}>PUBLISH</button></Link></li></div>
                         <input className="cancel" type="button" value="CANCEL" />
-                        <input className="publish1" type="button" value="PUBLISH"/>
+                        <input className="publish1" type="submit" value="PUBLISH"/>
                     </div>
         </section>
+    </>
     )
+    }
 }
 
 export default Upload;
